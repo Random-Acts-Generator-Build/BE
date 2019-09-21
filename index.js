f70d0f5c7cc8a89c8fe4052a9d8004cc757dc223
Natalie McCroy <nataliemccroy@gmail.com>
2019-09-20 21:14:12 -0500

    login, register, get users enpoints working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 
-const server = require('./api/server.js');
+const server = require('./server.js');
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const server = express();
 
+const UserRouter = require('./users/users-router')
+
 server.use(express.json());
+
+server.use('/api/users', UserRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({ api: 'up' });
