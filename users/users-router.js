36c3557338b50427d9a5f6b0986a3654c3d59932
Natalie McCroy <nataliemccroy@gmail.com>
2019-09-24 03:06:11 -0500

    GET /users/:id endpoint functions

--- users/users-router.js
+++ users/users-router.js
@@ -44,5 +44,21 @@ router.post('/login', (req, res) => {
 			res.status(500).json(error);
 		});
 });
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Users.findById(id)
+  .then(user => {
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: 'Could not find user with given id.' })
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get user' });
+  });
+});
   
 module.exports = router;
