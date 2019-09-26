12a2b9af83ff87d29b2265e4a1af52037e3131ff
Natalie McCroy <nataliemccroy@gmail.com>
2019-09-25 21:45:41 -0500

    added auth middleware, used locally on GET /users endpoint

--- users/users-router.js
+++ users/users-router.js
@@ -2,48 +2,17 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 
 const Users = require('./users-model');
+const restricted = require('../auth/auth-middleware')
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
-
-// router.post('/register', (req, res) => {
-//     const creds = req.body;
-//     const hash = bcrypt.hashSync(creds.password, 10);
-//     creds.password = hash;
-  
-//     Users.add(creds)
-//       .then(user => {
-//         res.status(201).json(user);
-//       })
-//       .catch(error => {
-//         res.status(500).json(error);
-//       });
-//   });
-  
-// router.post('/login', (req, res) => {
-// 	const { username, password } = req.body;
-
-// 	Users.findBy({ username })
-// 		.first()
-// 		.then(user => {
-// 			if (user && bcrypt.compareSync(password, user.password)) {
-// 				res.status(200).json({
-// 					message: `Welcome ${user.username}!`,
-// 				});
-// 			} else {
-// 				res.status(401).json({ message: 'Invalid Credentials' });
-// 			}
-// 		})
-// 		.catch(error => {
-// 			res.status(500).json(error);
-// 		});
-// });
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
